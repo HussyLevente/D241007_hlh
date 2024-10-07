fb5ffff1957eef8290b990fd65f198fc5226d0c8
hussy.levente <hussy.levente@28-DIAK8>
2024-10-07 12:39:03 +0200

    Add project files.

---
+++ D241007_hlh/Program.cs
@@ -0,0 +1,42 @@
+﻿using D241007_hlh;
+using System.Linq;
+using System.Text;
+
+List<Triatlon> versenyzok = [];
+
+using StreamReader sr = new(@"forras.txt", Encoding.UTF8);
+
+while (!sr.EndOfStream)
+{
+    versenyzok.Add(new(sr.ReadLine()));
+}
+
+Console.WriteLine($"Ennyi versenyző fejezte be a versenyt: {versenyzok.Count()}");
+
+
+var f1 = versenyzok.Count(r => r.KorKategoria == "elit junior");
+Console.WriteLine($"A versenyzők száma elit junior kategóriában: {f1}");
+
+
+var f2 = versenyzok.Where(p => p.Nem == "f").Average(p => DateTime.Now.Year - p.SzulEv);
+Console.WriteLine($"Férfi versenyzők átlag életkora: {f2:0.00} év");
+
+
+int f3 = versenyzok.Sum(r => r.Futas);
+Console.WriteLine($"A versenyzők futással töltött összideje órában: {f3:0.00}");
+
+
+int f4 = versenyzok.Where(p => p.KorKategoria == "20-24").Average(r => r.Uszas);
+Console.WriteLine($"Átlagos úszási idő 20-24 kategóriában: {f4}");
+
+
+var f5 = versenyzok.Where(p => p.Nem == "n").Min(p => p.Uszas);
+Console.WriteLine($"Női győztes: {f5} ");
+
+
+var f6 = versenyzok.GroupBy(r => r.Nem);
+Console.WriteLine("Nemenként a versenyt befejezők száma:");
+foreach (var sextypesGroup in f6)
+{
+    Console.WriteLine($"\t{sextypesGroup.Key} {sextypesGroup.Count()} db");
+}
---
+++ D241007_hlh/Triatlon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D241007_hlh
+{
+    internal class Triatlon
+    {
+        public string Nev { get; set; }
+        public int SzulEv { get; set; }
+        public int RajtSzam { get; set; }
+        public string Nem { get; set; }
+        public string KorKategoria { get; set; }
+        public DateTime Uszas {  get; set; }
+        public DateTime Depo { get; set; }
+        public DateTime Kerekpar { get; set; }
+        public DateTime MasoDepo { get; set; }
+        public DateTime Futas { get; set; }
+        public DateTime Ossz {  get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nev}, {SzulEv}, {RajtSzam}, {Nem}, {KorKategoria}, {Uszas}, {Depo}, {Kerekpar}, {MasoDepo}, {Futas}";
+        }
+
+        //public void Osszeg()
+        //{
+        //    int Osszegd = Uszas + Depo + Kerekpar + MasoDepo + Futas;
+        //}
+
+        public Triatlon(string row)
+        {
+            var v = row.Split(';');
+            Nev = v[0];
+            SzulEv = Convert.ToInt32(v[1]);
+            RajtSzam = Convert.ToInt32(v[2]);
+            Nem = v[3];
+            KorKategoria = v[4];
+            Uszas = Convert.ToDateTime(v[5]);
+            Depo = Convert.ToDateTime(v[6]);
+            Kerekpar = Convert.ToDateTime(v[7]);
+            MasoDepo = Convert.ToDateTime(v[8]);
+            Futas = Convert.ToDateTime(v[9]);
+        }
+    }
+}
